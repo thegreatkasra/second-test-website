df1372670de2d795c0351c161b8bdb06b354104f
kasra torabi <Kasra.torabiii@gmail.com>
2023-08-09 23:21:51 +0430

    a

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
  
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # tags = 
-    # category = 
+    tags = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(max_length=255, blank=True, null=True)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
