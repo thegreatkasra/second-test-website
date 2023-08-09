582bd2bc67b3c28032a60ecc2f8a725649367b4e
kasra torabi <Kasra.torabiii@gmail.com>
2023-08-09 16:39:23 +0430

    blog
    blog  url.py -views.py changes!

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path    
-from blog.views import blog_view ,blog_single
+from blog.views import blog_view ,blog_single 
+
 app_name = 'blog'
 
 urlpatterns = [
     path('index_blog',blog_view, name='index_blog'),
     path('single',blog_single, name='single'),
-
 ]
