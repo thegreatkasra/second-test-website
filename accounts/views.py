f640aca7e2706e55f9810036e1f089eada76a419
kasra torabi <Kasra.torabiii@gmail.com>
2023-09-01 08:03:03 +0430

    ./

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate , login ,logout
+from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'user authenticated as{request.user.username}'
-    else:
-        msg = 'user is not authenticated'
-    return render(request,'accounts/login.html', {'msg':msg})
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                if username and password:
+                    user = authenticate(request, username=username, password=password)
+                    if user is not None:
+                        login(request, user)
+                        return redirect('/')
+                    else:
+                        # Handle authentication failure
+                        return render(request, 'accounts/login.html', {'error_message': 'Invalid username or password'})
+        form = AuthenticationForm()
+        context = {'form':form}
+        return render(request, 'accounts/login.html', context)
+
 
 
 def logout_view(request):
